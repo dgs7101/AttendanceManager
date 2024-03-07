67e4a92e88c77e233d74fdfd57bb46bd5905cae7
dgs7101 <daigo.t.shimizu@gmail.com>
2024-03-07 16:31:38 +0900

    add username and current clock

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,14 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
     before_action :configure_permitted_parameters, if: :devise_controller?
 
     protected
 
     def configure_permitted_parameters
-        # 管理者用のカラムを許可
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:role])
-        devise_parameter_sanitizer.permit(:account_update, keys: [:role])
+        added_attrs = [ :email, :username, :password, :password_confirmation ]
+        devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
+        devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+        devise_parameter_sanitizer.permit :sign_in, keys: added_attrs
     end
 
     def after_sign_in_path_for(resource)
---
+++ app/javascript/clock.js
@@ -0,0 +1,25 @@
+document.addEventListener('turbolinks:load', function() {
+    startClock();
+});
+
+export function startClock() {
+    const dateElement = document.getElementById('date-display');
+    const timeElement = document.getElementById('time-display');
+    const dayOfWeekElement = document.getElementById('day-of-week-display');
+    
+    const daysOfWeek = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"];
+
+    function updateClock() {
+    const now = new Date();
+    const dateString = now.toLocaleDateString();
+    const timeString = now.toLocaleTimeString();
+    const dayOfWeekString = daysOfWeek[now.getDay()];
+
+    dateElement.textContent = dateString;
+    timeElement.textContent = timeString;
+    dayOfWeekElement.textContent = dayOfWeekString;
+    }
+    
+    setInterval(updateClock, 1000);
+    updateClock();
+}
