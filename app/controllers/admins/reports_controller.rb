0b89333b2af59d375905b47041cde32d95267b08
dgs7101 <daigo.t.shimizu@gmail.com>
2024-03-11 16:54:33 +0900

    modify report page

--- app/controllers/admins/reports_controller.rb
+++ app/controllers/admins/reports_controller.rb
@@ -6,7 +6,7 @@ module Admins
       def index
         @search = TimeRecord.ransack(params[:q])
         records = @search.result.order(check_in: :desc)
-        @all_records =  records.page(params[:page])
+        @all_records = records.page(params[:page])
         respond_to do |format|
           format.html
           format.csv { send_reports_csv(records) }
@@ -18,9 +18,9 @@ module Admins
           filename = "出退勤履歴一覧-#{Date.today}.csv"
     
           csv_data = CSV.generate(headers: true) do |csv|
-            csv << ['社員名', '出勤時刻', '退勤時刻']
+            csv << ['ID', '社員名', '出勤日', '出勤時刻', '退勤時刻', '勤務時間(秒)']
             records.each do |record|
-              csv << [record.user.username, record.check_in, record.check_out]
+              csv << [record.user_id, record.user.username, record.check_in&.strftime("%Y/%m/%d"), record.check_in&.strftime("%H:%M"), record.check_out&.strftime("%H:%M"), record.worked_seconds]
             end
           end
           send_data csv_data, filename: filename, type: :csv
